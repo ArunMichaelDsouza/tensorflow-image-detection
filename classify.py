6cd2280920212e5a5e059c3731e7f5443e9c363d
ArunMichaelDsouza <amdsouza92@gmail.com>
2017-05-27 19:27:06 +0530

    Classification script bug fixes

--- classify.py
+++ classify.py
@@ -8,10 +8,10 @@ image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
 # Loads label file, strips off carriage return
 label_lines = [line.rstrip() for line 
-                   in tf.gfile.GFile("retrained_labels.txt")]
+                   in tf.gfile.GFile("tf_files/retrained_labels.txt")]
 
 # Unpersists graph from file
-with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
+with tf.gfile.FastGFile("tf_files/retrained_graph.pb", 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
